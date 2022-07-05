d611b17179b269c91206d5ef28b4f23274597c9b
eva <eva.gupta1987@gmail.com>
2022-07-05 16:15:13 -0400

    cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "32um25",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
   },
 });
